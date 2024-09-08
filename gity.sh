1edc0556d49800d7fd3d1fec3dafb0d378f914e7
youssef <xyoussefx1500@gmail.com>
2024-09-08 11:36:43 +0200

    gity should function

---
+++ gity.sh
@@ -0,0 +1,42 @@
+#!/bin/bash	
+
+PKM="pacman"
+
+# Updates package manager
+echo "Updating package manager"
+sudo $PKM -Sy
+
+# Installs desired programs
+echo "Installing Git and Github CLI"
+sudo $PKM -S git github-cli
+
+echo "Email and name are required to configure git"
+
+echo "Enter your name "
+read -r NAME
+
+echo "Enter your email address "
+read -r EMAIL 
+
+echo "Choose default editor (ensure it is installed) e.g. code, nvim... "
+read -r EDITOR 
+
+echo "Configuring Git ..."
+git config --global user.name "$NAME"
+git config --global user.email "$EMAIL"
+git config --global core.editor "$EDITOR"
+git config --global color.ui auto
+
+# Display the current configuration
+echo "Git configuration complete. Use \"git config --list\" to see the current configuration."
+
+# List the current git configuration
+git config --list
+
+# Starts auth process for github-cli on browser
+gh auth login
+	
+# Sets the default editor for the CLI
+gh config set editor "$EDITOR --wait"
+
+echo "Setup complete. You can use "
